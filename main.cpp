3b6e9330028f0f68ed25a40342f0baa43cf0078a
wangyumeng <771535871@qq.com>
2020-12-29 21:25:44 +0800

    cs1

---
+++ main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  图书管理系统 图书管理
+//
+//  Created by 王雨梦 on 2020/12/29.
+//
+
+#include <iostream>
+using namespace std;
+
+void menu()
+{
+    cout << "---------------【name 图书管理系统】--------------" << endl ;
+    cout << "\t\t1." << endl ;
+    
+}
+
+int main() {
+    
+    cout << " djasd";
+//    system("pause");
+    system("clear");
+    
+    return 0;
+}
