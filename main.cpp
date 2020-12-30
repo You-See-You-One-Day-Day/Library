fb247d43a39066d9145ffbe31817da5ba06924eb
wuqiquan <2072219876@qq.com>
2020-12-30 22:55:22 +0800

    完善用户模式大致架构

--- main.cpp
+++ main.cpp
@@ -6,20 +6,20 @@
 //
 
 #include <iostream>
+
 using namespace std;
 
-void menu()
-{
-    cout << "---------------【name 图书管理系统】--------------" << endl ;
-    cout << "\t\t1." << endl ;
-    
+void menu() {
+    cout << "---------------【name 图书管理系统】--------------" << endl;
+    cout << "\t\t1." << endl;
+
 }
 
 int main() {
-    
+
     cout << " djasd";
 //    system("pause");
     system("clear");
-    
+
     return 0;
 }
